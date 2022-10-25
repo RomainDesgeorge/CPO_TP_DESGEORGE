f9bca1e445d34698f4a112525bc0e9dd41ebbd7e
romain desgeorge <romain.desgeorge@epfedu.fr>
2022-10-25 16:01:37 +0100

    commentaires rentrés

--- TP2_Biere_DESGEORGE/src/tp2_biere_desgeorge/TP2_Biere_DESGEORGE.java
+++ TP2_Biere_DESGEORGE/src/tp2_biere_desgeorge/TP2_Biere_DESGEORGE.java
@@ -14,20 +14,26 @@ public class TP2_Biere_DESGEORGE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",7.0,"Dubuisson") ;    
-uneBiere.lireEtiquette();
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",7.0,"Dubuisson") ;//Création de "uneBiere"    
+uneBiere.lireEtiquette(); //Lire "uneBiere"
 
-BouteilleBiere AutreBiere = new BouteilleBiere("Leffe",6.6,"Abbaye de Leffe") ;
-AutreBiere.lireEtiquette();
+BouteilleBiere AutreBiere = new BouteilleBiere("Leffe",6.6,"Abbaye de Leffe") ;//Création de "AutreBiere"
+AutreBiere.lireEtiquette();//Lire "AutreBiere"
 
-BouteilleBiere deuxBiere = new BouteilleBiere("Desperados",5.9,"Tequila flavour") ;
-deuxBiere.lireEtiquette();
+BouteilleBiere deuxBiere = new BouteilleBiere("Desperados",5.9,"Tequila flavour") ;//Création de "deuxBiere"
+deuxBiere.lireEtiquette();//Lire "deuxBiere"
 
-BouteilleBiere troisBiere = new BouteilleBiere("Pelford",5.8,"Mon frigo") ;
-troisBiere.lireEtiquette();
+BouteilleBiere troisBiere = new BouteilleBiere("Pelford",5.8,"Mon frigo") ;//Création de "troisBiere"
+troisBiere.lireEtiquette();//Lire "troisBiere"
 
-BouteilleBiere quatreBiere = new BouteilleBiere("La Chouffe",8.0,"Nain sur un vélo") ;
-quatreBiere.lireEtiquette();
+BouteilleBiere quatreBiere = new BouteilleBiere("La Chouffe",8.0,"Nain sur un vélo") ;//Création de "quatreBiere"
+quatreBiere.lireEtiquette();//Lire "quatreBiere"
+
+quatreBiere.decapsuler();
+
+System.out.println(uneBiere) ;
+
+uneBiere.decapsuler();
 // TODO code application logic here
     }
     
