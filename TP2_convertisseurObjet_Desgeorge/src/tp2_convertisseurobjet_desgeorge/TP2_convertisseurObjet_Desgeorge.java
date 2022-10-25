44d8a761eade54aa7e831147793bef9f04f10941
romain desgeorge <romain.desgeorge@epfedu.fr>
2022-10-25 15:56:23 +0100

    TP2 exo14

---
+++ TP2_convertisseurObjet_Desgeorge/src/tp2_convertisseurobjet_desgeorge/TP2_convertisseurObjet_Desgeorge.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_desgeorge;
+
+/**
+ *
+ * @author rom
+ */
+public class TP2_convertisseurObjet_Desgeorge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Convertisseur Temperature = new Convertisseur(); 
+        Temperature = FarenheitVersKelvin(Temperature);
+        Convertisseur Temperature2 = new Convertisseur(); 
+        Temperature2 = CelciusVersFarenheit(Temperature2);
+        
+        
+        // TODO code application logic here
+    }
+    
+}
