6a3eb458d53dbfeb10619798abf48f7338783567
romain desgeorge <romain.desgeorge@epfedu.fr>
2022-10-28 13:33:01 +0100

    TP2 Partie 2 exo 5 avec les commentaires

--- TP2_convertisseurObjet_Desgeorge/src/tp2_convertisseurobjet_desgeorge/TP2_convertisseurObjet_Desgeorge.java
+++ TP2_convertisseurObjet_Desgeorge/src/tp2_convertisseurobjet_desgeorge/TP2_convertisseurObjet_Desgeorge.java
@@ -21,36 +21,36 @@ public class TP2_convertisseurObjet_Desgeorge {
         double temp=0.0;
         System.out.println("Si vous êtes en celcisus tapez 1, \n Si vous êtes en Kelvin tapez 2, \n Si vous êtes en Farenheit tapez 3");
         Scanner sc = new Scanner(System.in);
-        int convers_depart = sc.nextInt();
-        if (convers_depart==1){
+        int convers_depart = sc.nextInt();//Demander de quelle unité on part
+        if (convers_depart==1){//Savoir si on part de Celcius
             System.out.println("Si vous voulez convertir en Kelvin tapez 1, \n Si vous voulez convertir en Farenheit tapez 2");
-            int convers_arrive =sc.nextInt();
-            if (convers_arrive==1){
+            int convers_arrive =sc.nextInt();//Demander dans quelle unité on veut convertir
+            if (convers_arrive==1){//Savoir si veut convertir en Kelvin
                 Temperature.CelciusVersKelvin(temp);
             }
-            else{
+            else{//Savoir si veut convertir en Farenheit
                Temperature.CelciusVersFarenheit(temp); 
             }
         }
         
-        if (convers_depart==2){
+        if (convers_depart==2){//Savoir si on part de Kelvin
             System.out.println("Si vous voulez convertir en Celcius tapez 1, \n Si vous voulez convertir en Farenheit tapez 2");
-            int convers_arrive =sc.nextInt();
-            if (convers_arrive==1){
+            int convers_arrive =sc.nextInt();//Demander dans quelle unité on veut convertir
+            if (convers_arrive==1){//Savoir si veut convertir en Celcius
                 Temperature.KelvinVersCelcius(temp);
             }
-            else{
+            else{//Savoir si veut convertir en Farenheit
                Temperature.KelvinVersFarenheit(temp); 
             }
         }
         
-        if (convers_depart==3){
+        if (convers_depart==3){//Savoir si on part de Farenheit
             System.out.println("Si vous voulez convertir en Celcius tapez 1, \n Si vous voulez convertir en Kelvin tapez 2");
-            int convers_arrive =sc.nextInt();
-            if (convers_arrive==1){
+            int convers_arrive =sc.nextInt();//Demander dans quelle unité on veut convertir
+            if (convers_arrive==1){//Savoir si veut convertir en Celcius
                 Temperature.FarenheitVersCelcius(temp);
             }
-            else{
+            else{//Savoir si veut convertir en Kelvin
                Temperature.FarenheitVersKelvin(temp); 
             }
         }
