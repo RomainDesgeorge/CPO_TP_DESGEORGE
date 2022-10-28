63ad7c58438ecc11c3772f1f80da533a2295cb2b
romain desgeorge <romain.desgeorge@epfedu.fr>
2022-10-28 15:56:21 +0100

    TP2 partie4 exercice 10

---
+++ TP2_convertisseurObjet_Desgeorge/src/tp2_convertisseurobjet_desgeorge/Convertisseur.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_desgeorge;
+
+/**
+ *
+ * @author rom
+ */
+public class Convertisseur {
+    public Convertisseur() {
+        nbConversions=0;
+    }
+int nbConversions;
+
+public double CelciusVersKelvin(double celcius){
+        
+        double Kelvin = celcius + 273.15;
+        nbConversions +=1;
+        
+        return Kelvin;
+        
+    }
+    
+    
+    public double KelvinVersCelcius(double kelvin){
+
+        double celcius = kelvin - 273.15;
+        nbConversions +=1;
+
+        return celcius;
+        
+    }
+    
+    
+    public double FarenheitVersCelcius(double farenheit){
+
+        double celcius = (farenheit - 32) / 1.8;
+        nbConversions +=1;
+
+        return celcius;
+
+    }
+
+
+    public double CelciusVersFarenheit(double celcius){
+
+        double farenheit = (celcius * 1.8) + 32;
+        nbConversions +=1;
+
+        return farenheit;
+        
+    }
+    
+    
+    public double KelvinVersFarenheit(double kelvin){
+
+        double farenheit = CelciusVersFarenheit(KelvinVersCelcius(kelvin));
+        nbConversions +=1;
+
+        return farenheit;
+        
+    }
+    
+    
+    public double FarenheitVersKelvin(double farenheit){
+
+        double kelvin = CelciusVersKelvin(FarenheitVersCelcius(farenheit));
+        nbConversions +=1;
+
+        return kelvin;
+    }
+    
+    @Override
+public String toString () {
+return "nb de conversions"+ nbConversions;
+}
+}
