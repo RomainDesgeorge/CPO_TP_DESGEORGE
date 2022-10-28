97712496f83d6fe8aa2dbd7aa361f3913bf9b505
romain desgeorge <romain.desgeorge@epfedu.fr>
2022-10-28 14:31:30 +0100

    TP2 Partie 4 question 4

--- TP2_relation_1_DESGEORGE/src/tp2_relation_1_desgeorge/TP2_relation_1_DESGEORGE.java
+++ TP2_relation_1_DESGEORGE/src/tp2_relation_1_desgeorge/TP2_relation_1_DESGEORGE.java
@@ -14,6 +14,14 @@ public class TP2_relation_1_DESGEORGE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    Personne bob = new Personne("Bobby", "Sixkiller");
+    Personne reno = new Personne("Reno", "Raines");
+    Voiture uneClio = new Voiture ("Clio", "Renault", 5,bob) ;
+    Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5, bob ) ;
+    Voiture une2008 = new Voiture ("2008", "Peugeot", 6, reno ) ;
+    Voiture uneMicra = new Voiture ("Micra", "Nissan", 4, reno ) ;
+
+    System.out.println("liste des voitures disponibles \n"+ uneClio +"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
         // TODO code application logic here
     }
     
