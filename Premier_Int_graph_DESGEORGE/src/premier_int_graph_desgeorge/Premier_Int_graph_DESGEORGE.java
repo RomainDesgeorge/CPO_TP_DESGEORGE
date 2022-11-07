a13e0ab2d4cad34f50343dd8eb36dab9ec54d497
romain desgeorge <romain.desgeorge@epfedu.fr>
2022-11-07 11:39:08 +0100

    Creation d'un nouveau projet graphique

---
+++ Premier_Int_graph_DESGEORGE/src/premier_int_graph_desgeorge/Premier_Int_graph_DESGEORGE.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package premier_int_graph_desgeorge;
+
+/**
+ *
+ * @author rom
+ */
+public class Premier_Int_graph_DESGEORGE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
