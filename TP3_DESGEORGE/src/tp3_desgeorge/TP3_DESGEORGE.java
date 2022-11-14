c18f49aa4b1629e8da3f67e8ababb25af84d047c
romain desgeorge <romain.desgeorge@epfedu.fr>
2022-11-14 10:21:52 +0100

    Création du Tableau qui regroupe les 2 batons et les deux Epees

--- TP3_DESGEORGE/src/tp3_desgeorge/TP3_DESGEORGE.java
+++ TP3_DESGEORGE/src/tp3_desgeorge/TP3_DESGEORGE.java
@@ -4,6 +4,8 @@
  */
 package tp3_desgeorge;
 
+import java.util.Vector;
+
 /**
  *
  * @author rom
@@ -17,7 +19,18 @@ public class TP3_DESGEORGE {
         Epee Excalibur = new Epee("Excalibur",7,5);
         Epee Durandal = new Epee("Durandal",4,7);
         Baton Chene = new Baton("Chene",4,5);
-        Baton Charme = new Baton("Chene",5,6);// TODO code application logic here
+        Baton Charme = new Baton("Charme",5,6);// TODO code application logic here
+        
+        Vector Tab=new Vector();
+        Tab.addElement(Excalibur);
+        Tab.addElement(Durandal);
+        Tab.addElement(Chene);
+        Tab.addElement(Charme);
+        int tailleTab = Tab.size();
+        for(int i=0;i<tailleTab;i++){
+        System.out.println(Tab.elementAt(i));
+        }
+                
     }
     
 }
