065e59665b684d271b1bc42650e1ba87999c2cd1
romain desgeorge <romain.desgeorge@epfedu.fr>
2022-11-14 10:02:08 +0100

    Definition de baton

---
+++ TP3_DESGEORGE/src/tp3_desgeorge/Baton.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_desgeorge;
+
+/**
+ *
+ * @author rom
+ */
+public class Baton extends Armes{
+    int age;
+    public Baton(String n, int i, int a){
+        super(n,i);
+        if(a>0){
+            age=a;
+        } else{
+            age=0;
+        }
+    }
+}
