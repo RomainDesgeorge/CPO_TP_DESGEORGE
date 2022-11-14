5cf787fda5523474066dd6a8526d0bf5382a4c39
romain desgeorge <romain.desgeorge@epfedu.fr>
2022-11-14 10:51:06 +0100

    Finition de la classe Personnage

--- TP3_DESGEORGE/src/tp3_desgeorge/Personnage.java
+++ TP3_DESGEORGE/src/tp3_desgeorge/Personnage.java
@@ -9,5 +9,27 @@ package tp3_desgeorge;
  * @author rom
  */
 public class Personnage {
+    public Personnage(String unNom, int att){
+        Nom = unNom;//prendre en donnée unNom et le transformer en Nom
+        niv_vie = att;//introduire à la variable nbVoiture 0
+       
+    }
+    String Nom;
+    private int niv_vie ;   
+    
+    public int getNiv_vie() {
+        return niv_vie;
+    }
+    
+    public void setNiv_vie(int niv_vie) {
+        if (niv_vie>0){
+        this.niv_vie = niv_vie;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Armes{" + "Nom=" + Nom + ", niv_vie=" + niv_vie+ '}';
+    }
     
 }
